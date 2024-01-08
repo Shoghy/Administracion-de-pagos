5ec04a72c4a7248ea82681a3b24e1a3e9468669c
Shoghy Martinez <shoghymartinez@gmail.com>
2024-01-08 15:03:24 -0400

    🚧Test index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import IndexPage from "./pages";
+
 export default function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={IndexPage}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
